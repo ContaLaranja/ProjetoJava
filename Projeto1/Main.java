f0078bc31a2e5ed76423a97dbfae5ea0e56ee521
ContaLaranja <alexsandro.s.junior6@ba.estudante.senai.br>
2025-02-21 22:26:26 +0000

    Estrutura

--- Projeto1/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PaiDePet cliente3 = new PaiDePet("xande", "20", 
-       new Pet("Rex", "4", "PitBull"));
-
-       System.out.println(cliente3.toString());
-    }
-}
